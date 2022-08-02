ee67fbaae3c2f56b273e9cdf703013e874e1c1ea
ThiaudioTT <thiago.sky25@gmail.com>
2022-08-02 16:58:46 -0300

    solved internal collisions and determined a board, close #7

--- src/main.cpp
+++ src/main.cpp
@@ -8,8 +8,8 @@ constexpr int windowHeight = 600;
 int main()
 {
    sf::RenderWindow window(sf::VideoMode(windowWidth, windowHeight), "Snake");
-   window.setFramerateLimit(30u);
-
+   window.setFramerateLimit(30);
+   window.setKeyRepeatEnabled(false);
 
    Snake snake;
    while (window.isOpen())
@@ -17,39 +17,35 @@ int main()
       sf::Event event;
       while (window.pollEvent(event)) // deal with event keyboards and close window
       {
-         if (event.type == sf::Event::Closed || sf::Keyboard::isKeyPressed(sf::Keyboard::Escape))
+         if (event.type == sf::Event::Closed) // || sf::Keyboard::isKeyPressed(sf::Keyboard::Escape))
          {
             window.close();
             return 0;
          }
-         else if (sf::Event::KeyPressed) // others keyboards
+         else if (event.type == sf::Event::KeyPressed) // others keys
          {
             switch (event.key.code)
             {
             case sf::Keyboard::W: // up
                std::cout << "W PRESSED" << std::endl;
-               snake.move(0);
                snake.direction = 0;
                break;
 
             case sf::Keyboard::D:
                std::cout << "D PRESSED" << std::endl;
-               snake.move(1);
                snake.direction = 1;
                break;
 
             case sf::Keyboard::S:
                std::cout << "S PRESSED" << std::endl;
-               snake.move(2);
                snake.direction = 2;
                break;
 
             case sf::Keyboard::A: // left
                std::cout << "A PRESSED" << std::endl;
-               snake.move(3);
                snake.direction = 3;
                break;
-               
+
             case sf::Keyboard::Space: // FOR DEBUG
                std::cout << "SPACE PRESSED" << std::endl;
                snake.grow();
@@ -58,7 +54,7 @@ int main()
                break;
             }
          }
-      } 
+      }
 
       snake.move(snake.direction);
 
@@ -66,5 +62,6 @@ int main()
       window.clear(sf::Color::Black);
       snake.draw(window);
       window.display();
+      sf::sleep(sf::milliseconds(100));
    }
 }
